7becbd7e9faba04617233552e3f9a99f872224d1
码匠君 <pointer_v@qq.com>
2022-02-14 10:54:22 +0800

    🏁 fix: v2.7.0.Beta2
    1. 修改文档描述错误
    2. 优化自定义 Hibernate Dialect，增加对 CLOB 和 BLOB 的统一支持

--- engine-data/data-sdk-jpa/src/main/java/cn/herodotus/engine/data/jpa/hibernate/postgresql/HerodotusPostgreSQLDialect.java
+++ engine-data/data-sdk-jpa/src/main/java/cn/herodotus/engine/data/jpa/hibernate/postgresql/HerodotusPostgreSQLDialect.java
@@ -25,7 +25,10 @@
 
 package cn.herodotus.engine.data.jpa.hibernate.postgresql;
 
-import org.hibernate.dialect.PostgreSQL9Dialect;
+import org.hibernate.dialect.PostgreSQL10Dialect;
+import org.hibernate.type.descriptor.sql.LongVarbinaryTypeDescriptor;
+import org.hibernate.type.descriptor.sql.LongVarcharTypeDescriptor;
+import org.hibernate.type.descriptor.sql.SqlTypeDescriptor;
 
 import java.sql.Types;
 
@@ -35,7 +38,7 @@ import java.sql.Types;
  * @author : gengwei.zheng
  * @date : 2019/11/15 10:31
  */
-public class HerodotusPostgreSQLDialect extends PostgreSQL9Dialect {
+public class HerodotusPostgreSQLDialect extends PostgreSQL10Dialect {
 
     public HerodotusPostgreSQLDialect() {
         super();
@@ -43,19 +46,43 @@ public class HerodotusPostgreSQLDialect extends PostgreSQL9Dialect {
         this.registerColumnType(Types.ARRAY, "text[]");
     }
 
+//    /**
+//     * 重载getAddForeignKeyConstraintString方法，阻止ddl-auto添加外键关联
+//     *
+//     * @param constraintName
+//     * @param foreignKey
+//     * @param referencedTable
+//     * @param primaryKey
+//     * @param referencesPrimaryKey
+//     * @return String
+//     */
+//    @Override
+//    public String getAddForeignKeyConstraintString(String constraintName, String[] foreignKey, String referencedTable, String[] primaryKey, boolean referencesPrimaryKey) {
+//        //  设置foreignkey对应的列值可以为空
+//        return " alter " + foreignKey[0] + " set default null ";
+//    }
+
+
     /**
-     * 重载getAddForeignKeyConstraintString方法，阻止ddl-auto添加外键关联
+     * 在 JPA 环境下，映射 PostgreSQL TEXT 专有类型的三种处理方式中的一种。
+     * <p>
+     * 这个方法将 @Lob 对应的 CLOB 和 BLOB 合并进行了处理
      *
-     * @param constraintName
-     * @param foreignKey
-     * @param referencedTable
-     * @param primaryKey
-     * @param referencesPrimaryKey
-     * @return String
+     * @param sqlTypeDescriptor SQL 类型描述器
+     * @return 处理后的 SQL 类型描述器
+     * @see <a href="https://www.baeldung.com/jpa-annotation-postgresql-text-type">参考资料1</a>
+     * @see <a href="https://blog.csdn.net/weixin_30539835/article/details/98190592">参考资料2</a>
+     * @see <a href="https://stackoverflow.com/questions/13090089/org-hibernate-type-texttype-and-oracle">参考资料3</a>
+     * @see <a href="https://www.programminghunter.com/article/2482653094/">参考资料4</a>
      */
     @Override
-    public String getAddForeignKeyConstraintString(String constraintName, String[] foreignKey, String referencedTable, String[] primaryKey, boolean referencesPrimaryKey) {
-        //  设置foreignkey对应的列值可以为空
-        return " alter " + foreignKey[0] + " set default null ";
+    public SqlTypeDescriptor remapSqlTypeDescriptor(SqlTypeDescriptor sqlTypeDescriptor) {
+        switch (sqlTypeDescriptor.getSqlType()) {
+            case Types.CLOB:
+                return LongVarcharTypeDescriptor.INSTANCE;
+            case Types.BLOB:
+                return LongVarbinaryTypeDescriptor.INSTANCE;
+        }
+        return super.remapSqlTypeDescriptor(sqlTypeDescriptor);
     }
 }
