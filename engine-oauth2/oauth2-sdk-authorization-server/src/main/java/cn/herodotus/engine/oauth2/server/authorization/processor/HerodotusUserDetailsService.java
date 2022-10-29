2a520b802622b731817db298cc49afe0324314df
songlai <959009@qq.com>
2022-10-29 14:28:30 +0800

    修改注释中的错别字。

--- engine-oauth2/oauth2-sdk-authorization-server/src/main/java/cn/herodotus/engine/oauth2/server/authorization/processor/HerodotusUserDetailsService.java
+++ engine-oauth2/oauth2-sdk-authorization-server/src/main/java/cn/herodotus/engine/oauth2/server/authorization/processor/HerodotusUserDetailsService.java
@@ -39,7 +39,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
  * <p>Description: UserDetailsService核心类 </p>
  * <p>
  * 之前一直使用Fegin进行UserDetailsService的远程调用。现在直接改为数据库访问。主要原因是：
- * 1. 根据目前的设计，Oauth的表与系统权限相关的表是在一个库中的。因此UAA和UPMS分开是为了以后提高新能考虑，逻辑上没有必要分成两个服务。
+ * 1. 根据目前的设计，Oauth的表与系统权限相关的表是在一个库中的。因此UAA和UPMS分开是为了以后提高性能考虑，逻辑上没有必要分成两个服务。
  * 2. UserDetailsService 和 ClientDetailsService 是Oauth核心内容，调用频繁增加一道远程调用增加消耗而已。
  * 3. UserDetailsService 和 ClientDetailsService 是Oauth核心内容，只是UAA在使用。
  * 4. UserDetailsService 和 ClientDetailsService 是Oauth核心内容，是各种验证权限之前必须调用的内容。
